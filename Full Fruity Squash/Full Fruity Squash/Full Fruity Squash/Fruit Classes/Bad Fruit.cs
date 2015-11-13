61eed9686d2b3173cae10da0adaf55a67c0ea5cb
mrmoose182 <482721@FEN-052-076.adir.hull.ac.uk>
2015-11-13 21:46:43 +0000

    added classes and stuff

--- Full Fruity Squash/Full Fruity Squash/Full Fruity Squash/Fruit Classes/Bad Fruit.cs
+++ Full Fruity Squash/Full Fruity Squash/Full Fruity Squash/Fruit Classes/Bad Fruit.cs
@@ -18,19 +18,24 @@ namespace Full_Fruity_Squash
 
         badFruitState currentBadFruitState = badFruitState.badFalling;
 
-        public Bad_Fruit(Texture2D tex, Vector2 pos)
+        public Bad_Fruit(Texture2D tex, Vector2 pos) 
+            : base (tex, pos)
         {
+            texture = tex;
+            position = pos;
 
+            center = new Vector2(position.X + texture.Width / 2, position.Y + texture.Height / 2);
+            origin = new Vector2(texture.Width / 2, texture.Height / 2);
         }
 
-        public void Update()
+        public override void Update(GameTime gameTime)
         {
-
+            base.Update(gameTime);
         }
 
-        public void Draw()
+        public override void Draw(SpriteBatch spriteBatch)
         {
-
+            base.Draw(spriteBatch);
         }
     }
 }
