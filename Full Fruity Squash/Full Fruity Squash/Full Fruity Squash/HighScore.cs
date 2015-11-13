81cd9ccbdf96d31617f139e274dff62adccf057a
ADWilsonCode <andrew.wilson007@gmail.com>
2015-11-13 21:20:10 +0000

    High Score Class Load

---
+++ Full Fruity Squash/Full Fruity Squash/Full Fruity Squash/HighScore.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Full_Fruity_Squash
+{
+    public class HighScore
+    {
+        string filename = ("FullFruitySquash.txt");
+        List<PersonDetails> TopFivePeople = new List<PersonDetails>();
+
+        public HighScore() // Constructor
+        {
+            LoadPeople();
+        }
+
+        public bool LoadPeople() // Load people from text file
+        {
+
+            System.IO.TextReader TextIn = null;
+            try
+            {
+                TextIn = new System.IO.StreamReader(filename);
+
+                int NumberOfPerson = int.Parse(TextIn.ReadLine());
+                for (int i = 0; i < NumberOfPerson; i++) // Load all the high score people
+                {
+                    string PersonName = TextIn.ReadLine();
+                    int Score = int.Parse(TextIn.ReadLine());
+                    TopFivePeople.Add(new PersonDetails(PersonName, Score));
+                }
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (TextIn != null) TextIn.Close();
+            }
+
+            return true;
+        }
+
+        public bool SavePeople() // Save people from text file
+        {
+            return true;
+        }
+
+    }
+
+    class PersonDetails
+    {
+        public PersonDetails(string InName, int INScore)
+        {
+            Name = InName;
+            Score = INScore;
+        }
+
+        private string Name;
+
+        public string NameGetSet
+        {
+            get{return Name;}
+            set { Name = value; }
+        }
+
+        private int Score;
+
+        public int ScoreGetSet
+        {
+            get { return Score; }
+            set{ Score = value; }
+        }
+    }
+
+}
