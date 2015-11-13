c973094b83038eaf2463a495e891730425f9b855
ADWilsonCode <andrew.wilson007@gmail.com>
2015-11-13 20:26:29 +0000

    File Stucture Altered

--- Full Fruity Squash/Full Fruity Squash/Full Fruity Squash/Fruit Classes/Rotten Apple.cs
+++ Full Fruity Squash/Full Fruity Squash/Full Fruity Squash/Fruit Classes/Rotten Apple.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Full_Fruity_Squash.Fuit_Classes
+namespace Full_Fruity_Squash
 {
     public class Rotten_Apple : Bad_Fruit
     {
