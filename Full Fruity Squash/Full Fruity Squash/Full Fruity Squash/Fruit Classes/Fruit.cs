bcb7a9334068bfedf21cfddaa28c5a3eac6618d0
Stephen D Tyler <490905@hull.ac.uk>
2015-11-13 22:55:47 +0000

    Adding all complete fruit classes

--- Full Fruity Squash/Full Fruity Squash/Full Fruity Squash/Fruit Classes/Fruit.cs
+++ Full Fruity Squash/Full Fruity Squash/Full Fruity Squash/Fruit Classes/Fruit.cs
@@ -2,10 +2,85 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Full_Fruity_Squash
 {
     public class Fruit : Sprite
     {
+        public enum fruitState
+        {
+            falling,
+            caught,
+            dropped,
+        };
+
+        protected fruitState currentState;
+        protected bool isAlive;
+
+        public bool IsAlive
+        {
+            get { return isAlive; }
+            set { isAlive = value; }
+        }
+
+         public Fruit(Texture2D tex, Vector2 pos) 
+             : base(tex, pos)
+        {
+            texture = tex;
+            position = pos;
+            velocity = Vector2.Zero;
+            center = new Vector2(position.X + texture.Width / 2, position.Y + texture.Height / 2);
+            origin = new Vector2(texture.Width / 2, texture.Height / 2);
+            currentState = fruitState.falling;
+            isAlive = true;
+        }
+
+        protected virtual void Fall(GameTime gameTime)
+         {
+             velocity = new Vector2(0, 5);
+             position += velocity;
+         }
+
+        protected virtual void Caught(GameTime gameTime)
+        {
+            //TODO: add code to follow the position of the player
+        }
+
+        protected virtual void Dropped(GameTime gameTime)
+        {
+            velocity = new Vector2(0, 5);
+            position += velocity;
+        }
+
+        public virtual void Kill()
+        {
+            isAlive = false;
+        }
+
+         protected override void Update(GameTime gameTime)
+         {
+             switch(currentState)
+             {
+                 case fruitState.falling:
+                     Fall(gameTime);
+                     break;
+                 case fruitState.caught:
+                     Caught(gameTime);
+                     break;
+                 case fruitState.dropped:
+                     Dropped(gameTime);
+                     break;
+             }
+
+             base.Update(gameTime);
+         }
+
+         protected override void Draw(SpriteBatch spriteBatch)
+         {
+             base.Draw(spriteBatch);
+         }
     }
 }
--- Full Fruity Squash/Full Fruity Squash/Full Fruity Squash/Fruit Classes/Orange.cs
+++ Full Fruity Squash/Full Fruity Squash/Full Fruity Squash/Fruit Classes/Orange.cs
@@ -2,10 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Full_Fruity_Squash
 {
     public class Orange : Fruit
     {
+        public Orange(Texture2D tex, Vector2 pos)
+            : base(tex, pos)
+        {
+            texture = tex;
+            position = pos;
+            velocity = Vector2.Zero;
+            center = new Vector2(position.X + texture.Width / 2, position.Y + texture.Height / 2);
+            origin = new Vector2(texture.Width / 2, texture.Height / 2);
+            currentState = fruitState.falling;
+            isAlive = true;
+        }
+
+        public override void Fall(GameTime gameTime)
+        {
+            base.Fall(gameTime);
+        }
+
+        public override void Caught(GameTime gameTime)
+        {
+            base.Caught(gameTime);
+        }
+
+        public override void Dropped(GameTime gameTime)
+        {
+            base.Dropped(gameTime);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+        }
     }
 }
